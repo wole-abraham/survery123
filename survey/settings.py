20a91160b9e253351e1a19cbf23917110f3c6dc4
wole Abraham <wole1702@gmail.com>
2025-01-25 04:26:09 -0800

    added hos

--- survey/settings.py
+++ survey/settings.py
@@ -126,6 +126,6 @@ STATIC_URL = 'static/'
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 STATIC_URL = '/static/'
-STATIC_ROOT = 'home/devwole/survery123/static'
+STATIC_ROOT = '/home/devwole/survery123/static'
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
